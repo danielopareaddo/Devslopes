0bba659e09769c9d1a5882544740eb6ab8f206bb
danielopareaddo <danielopareaddo@yahoo.com>
2020-04-03 00:10:16 +0000

    lesson 4

---
+++ app/src/main/java/com/danielopareaddo/devprofile/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.danielopareaddo.devprofile
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+         rounded.cornerRadius = 15f
+        //rounded.isCircular = true .........use this to totally round the logo or any image
+        logo.setImageDrawable(rounded)
+
+
+
+    }
+}
